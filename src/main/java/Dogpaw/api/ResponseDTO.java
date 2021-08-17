1d0e4ecd53530a89205510fa1a1ce809eeedf33c
logicaltrojan <logicaltrojan@gmail.com>
2021-08-18 01:21:53 +0900

    missing file fix

---
+++ src/main/java/Dogpaw/api/ResponseDTO.java
@@ -0,0 +1,31 @@
+package Dogpaw.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+public class ResponseDTO {
+
+
+    @AllArgsConstructor
+    public static class BaseResponse {
+
+        Boolean success;
+    }
+
+    public static class Create extends BaseResponse{
+        Long id;
+
+        public Create(Long id, Boolean success){
+            super(success);
+            this.id = id;
+        }
+    }
+    public static class Delete extends BaseResponse{
+        public Delete(Boolean success) {
+            super(success);
+        }
+    }
+
+}
---
+++ src/main/java/Dogpaw/api/WorkSpaceDTO.java
@@ -0,0 +1,25 @@
+package Dogpaw.api;
+
+import lombok.Data;
+
+public class WorkSpaceDTO {
+
+    @Data
+    public static class Create extends Update{
+
+    }
+
+    @Data
+    public static class Update {
+
+        private String name;
+
+        private String url;
+
+    }
+
+    @Data
+    public static class Delete {
+        private Long id;
+    }
+}
